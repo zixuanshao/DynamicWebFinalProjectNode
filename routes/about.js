2a9b850ece6795b464e7c24f6c22e59afb5dd2bf
Claudia Shao <zxshao99@gmail.com>
2019-11-16 23:01:21 -0500

    dependencies installed

--- routes/about.js
+++ routes/about.js
@@ -2,6 +2,5 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => res.send('About the API'));
-router.get('/me', (req, res) => res.send('About Me'));
 
 module.exports = router;

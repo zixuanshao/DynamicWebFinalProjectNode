9012770c8bc1d4bbe24be8129962655fce117d48
Claudia Shao <zxshao99@gmail.com>
2019-12-04 17:37:10 -0500

    firebase

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => res.send('Welcome!'));
+
+const admin = require('firebase-admin');
+
+let serviceAccount = require('../serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dynamic-web-final.firebaseio.com"
+});
+
+let db = admin.firestore();
+
+router.get('/', function(req, res){
+    var users = [];
+    let usersRef = db.collection('users');
+    let allUsers = usersRef.get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+          // console.log(doc.id, '=>', doc.data());
+          //add content
+          users.push({
+            id: doc.id,
+            user_info: doc.data()
+          }) 
+        });
+
+        //display all the contents in the 'users' array
+        res.send(users)
+      })
+    .catch(err => {
+        console.log('Error getting documents', err);
+    });
+
+})
+
 
 module.exports = router;

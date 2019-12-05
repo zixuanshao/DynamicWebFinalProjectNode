37399f17224691057ec6883ea8cb306ee37b48c4
Claudia Shao <zxshao99@gmail.com>
2019-12-05 15:09:42 -0500

    enable cors

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express ();
 const port = process.env.PORT || 4000;
 
 const indexRoute = require('./routes/index.js')
-const aboutRoute = require('./routes/about.js')
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
-app.use('/', indexRoute)
-app.use('/about', aboutRoute)
+app.get('/', indexRoute)
 
 // serve static images
 // app.use('/static', express.static('public'))

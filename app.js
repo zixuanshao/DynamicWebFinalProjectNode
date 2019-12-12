4ca2464305697083680331f32bb31ca648129684
Claudia Shao <zxshao99@gmail.com>
2019-12-12 01:58:42 -0500

    updated cors control access

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const deleteRoute = require('./routes/delete.js')
 app.use(express.json())
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://dynamic-web-final.netlify.com/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
